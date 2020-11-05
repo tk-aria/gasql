205d445a39e79c766fdbcd270c69d633463ca23e
tkaria <tkaria.info@gmail.com>
2020-11-05 19:23:31 +0900

    add sample code

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,16 @@
 // src/main.ts
-import dayjs from "dayjs";
-import "dayjs/locale/ja";
-
-dayjs.locale("ja");
 
 export function doGet(): GoogleAppsScript.Content.TextOutput {
   return ContentService.createTextOutput("Hello World");
 }
 
 export function doPost(): GoogleAppsScript.Content.TextOutput {
-  const text = dayjs().format("YYYY年M月D日(ddd) H:m");
-
-  const data: SlackData = {
-    text,
+  const data = {
+    method: "hiogehoge",
     response_type: "ephemeral",
   };
 
   return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(
     ContentService.MimeType.JSON
   );
-}
-
-interface SlackData {
-  text: string;
-  response_type: "ephemeral" | "in_chanel";
 }
